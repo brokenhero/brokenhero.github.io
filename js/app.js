8b2fd9fb289d80cf4e8e53cc43b0dccf863d594f
simonpai <simonpai@potix.com>
2013-08-08 22:23:10 +0800

    1.0.0, post: dissolution effect.

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$(function () {
+	// image information
+	$('#op-info').tooltip();
+	
+	// hach post DOM
+	$('ul.conversation > li').wrapInner('<span />');
+	$('ul.act > li').wrapInner('<div><span /></div>');
+	
+	// hack hashtags
+	var headerHeight = $('#header').height();
+	$(body).on('click', '[href^=#]', function (e) {
+		var $target = $(this.getAttribute('href'));
+		e.preventDefault();
+		$(window).scrollTop($target.offset().top - headerHeight - 6);
+	});
+	
+	// wire references
+	$('#entry cite').map(function (index) {
+		return $('<li id="cite-note-' + (++index) + '"><a href="#cite-ref-' + index + '">^</a> ' + this.innerHTML + '</li>')[0];
+	}).appendTo('#references > ol');
+	
+	$('#entry cite').wrap(function (index) {
+		return '<sup id="cite-ref-' + (++index) + '" />';
+	}).before(function (index) {
+		return '<a class"cite-link" href="#cite-note-' + (++index) + '">[' + index + ']</a>';
+	});
+	
+	if ($('#references > ol').is(':empty'))
+		$('#references').detach();
+	
+	$('#cast').appendTo('#entry');
+	$('#last').appendTo('#entry');
+});
